a2d252f862d6bba737a494f0f349898014edde53
justinphan99 <phanquockhoa99@gmail.com>
2023-01-02 23:00:38 +0700

    feature/init-loans-service

---
+++ loans-service/src/main/java/com/bank/loansservice/LoansServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bank.loansservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoansServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoansServiceApplication.class, args);
+	}
+
+}
