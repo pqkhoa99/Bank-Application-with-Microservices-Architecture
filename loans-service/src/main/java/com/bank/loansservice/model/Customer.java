6e9a4edb22b27cdd47dd0154cb80c17fb7db57da
justinphan99 <phanquockhoa99@gmail.com>
2023-01-03 12:16:01 +0700

    ADD model Loans and Customer for loans-service

---
+++ loans-service/src/main/java/com/bank/loansservice/model/Customer.java
@@ -0,0 +1,12 @@
+package com.bank.loansservice.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter @Setter @ToString
+public class Customer {
+
+    private int customerId;
+
+}
