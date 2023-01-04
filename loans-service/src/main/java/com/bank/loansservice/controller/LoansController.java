befffc3a357982d914823c33b2872aed15c5e5ed
justinphan99 <phanquockhoa99@gmail.com>
2023-01-04 21:56:35 +0700

    ADD config-application for loans-service

--- loans-service/src/main/java/com/bank/loansservice/controller/LoansController.java
+++ loans-service/src/main/java/com/bank/loansservice/controller/LoansController.java
@@ -1,9 +1,15 @@
 package com.bank.loansservice.controller;
 
+import com.bank.loansservice.config.LoansServiceConfig;
 import com.bank.loansservice.model.Customer;
 import com.bank.loansservice.model.Loans;
+import com.bank.loansservice.model.Properties;
 import com.bank.loansservice.repository.LoansRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +22,9 @@ public class LoansController {
     @Autowired
     private LoansRepository loansRepository;
 
+    @Autowired
+    private LoansServiceConfig loansConfig;
+
     @PostMapping("/myLoans")
     public List<Loans> getLoansDetails(@RequestBody Customer customer) {
         List<Loans> loans = loansRepository.findByCustomerIdOrderByStartDtDesc(customer.getCustomerId());
@@ -24,5 +33,17 @@ public class LoansController {
         } else {
             return null;
         }
+    }
+
+    @GetMapping("/loans/properties")
+    public String getPropertyDetails() throws JsonProcessingException {
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        Properties properties = new Properties(
+                loansConfig.getMsg(),
+                loansConfig.getBuildVersion(),
+                loansConfig.getMailDetails(),
+                loansConfig.getActiveBranches());
+        String jsonStr = ow.writeValueAsString(properties);
+        return jsonStr;
     }
 }
